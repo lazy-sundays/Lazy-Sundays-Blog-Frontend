3c6c40018c4d3c7949158f0ac540c1fe06727449
jasperl <jasperl@vt.edu>
2023-05-09 18:00:01 -0400

    added read time calculator, added contact us, refined schema

---
+++ backend/lazy-sundays-blog/src/api/article/content-types/article/lifecycles.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { default: readTimeEstimate } = require('read-time-estimate');
+
+module.exports = {
+  
+    async afterCreate(event) {
+      const { result, params } = event;
+
+      //calculate and store time it takes to read
+      const entry = await strapi.entityService.update('api::article.article', result.id, {
+        data: { 
+          readTime: readTimeEstimate(result.body).duration.toFixed(2)
+        },
+      });
+
+    },
+
+    async afterUpdate(event) {
+        const { result, params } = event;
+
+        //calculate and store time it takes to read
+        const entry = await strapi.entityService.update('api::article.article', result.id, {
+          data: { 
+            readTime: readTimeEstimate(result.body).duration.toFixed(2)
+          },
+        });
+    },
+};
