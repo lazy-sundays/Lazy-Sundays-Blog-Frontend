6b423cf9eecd79ab5288acd92f7b9655ee95c03c
jasperl <jasperl@vt.edu>
2023-05-09 19:22:46 -0400

    put into a function

--- backend/lazy-sundays-blog/src/api/article/content-types/article/lifecycles.js
+++ backend/lazy-sundays-blog/src/api/article/content-types/article/lifecycles.js
@@ -2,28 +2,27 @@
 
 const { default: readTimeEstimate } = require('read-time-estimate');
 
+async function updateReadTime(event){
+  const { result, params } = event;
+
+  //calculate and store time it takes to read
+  const entry = await strapi.entityService.update('api::article.article', result.id, {
+    data: { 
+      readTime: readTimeEstimate(result.body).duration.toFixed(2)
+    },
+  });
+}
+
 module.exports = {
   
-    async afterCreate(event) {
-      const { result, params } = event;
+    afterCreate(event) {
 
-      //calculate and store time it takes to read
-      const entry = await strapi.entityService.update('api::article.article', result.id, {
-        data: { 
-          readTime: readTimeEstimate(result.body).duration.toFixed(2)
-        },
-      });
+      updateReadTime(event);
 
     },
 
-    async afterUpdate(event) {
-      const { result, params } = event;
-
-      //calculate and store time it takes to read
-      const entry = await strapi.entityService.update('api::article.article', result.id, {
-        data: { 
-          readTime: readTimeEstimate(result.body).duration.toFixed(2)
-        },
-      });
+    afterUpdate(event) {
+      
+      updateReadTime(event);
     },
 };
