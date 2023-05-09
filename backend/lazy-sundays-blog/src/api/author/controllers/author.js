42a47d33ef54d6eea119126602c7f3afaded3240
jenny leidig <jasperl@vt.edu>
2023-05-08 20:21:51 -0400

    added support for mutiple authors

--- backend/lazy-sundays-blog/src/api/author/controllers/author.js
+++ backend/lazy-sundays-blog/src/api/author/controllers/author.js
@@ -10,8 +10,8 @@ module.exports = createCoreController('api::author.author', {
     async countArticles(ctx){
 
         const [entries, count] = await strapi.db.query('api::article.article').findWithCount({
-            where: { author: {id:ctx.params.id}},
-            populate: { author: true },
+            where: { authors: {id:ctx.params.id}},
+            populate: { authors: true },
         });
         return count;
 
