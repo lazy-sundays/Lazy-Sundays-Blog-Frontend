78d329e04d0395c90e069810cc5f431d7fd64920
jasperl <jasperl@vt.edu>
2023-05-09 18:04:04 -0400

    fixed formatting

--- backend/lazy-sundays-blog/src/api/article/content-types/article/lifecycles.js
+++ backend/lazy-sundays-blog/src/api/article/content-types/article/lifecycles.js
@@ -17,13 +17,13 @@ module.exports = {
     },
 
     async afterUpdate(event) {
-        const { result, params } = event;
+      const { result, params } = event;
 
-        //calculate and store time it takes to read
-        const entry = await strapi.entityService.update('api::article.article', result.id, {
-          data: { 
-            readTime: readTimeEstimate(result.body).duration.toFixed(2)
-          },
-        });
+      //calculate and store time it takes to read
+      const entry = await strapi.entityService.update('api::article.article', result.id, {
+        data: { 
+          readTime: readTimeEstimate(result.body).duration.toFixed(2)
+        },
+      });
     },
 };
