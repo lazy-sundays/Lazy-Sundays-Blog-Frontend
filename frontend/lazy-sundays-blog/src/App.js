b8972d4de70ea5cd4b0575de741d6e502a020946
Devin Toms <dtoms@vt.edu>
2023-05-11 19:52:26 -0400

    added front end skeleton

--- frontend/lazy-sundays-blog/src/App.js
+++ frontend/lazy-sundays-blog/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Common/Navbar';
+import Footer from './Common/Footer';
+import Home from './Home/Home';
+import Error from './Error/Error';
+import AboutUs from './AboutUs/AboutUs';
+import Archive from './Archive/Archive';
+import Article from './Article/Article';
+import Author from './Author/Author';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/articles' element={<Archive />} />
+        <Route path='/articles/:id' element={<Article />} />
+        <Route path='/authors/:id' element={<Author />} />
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
