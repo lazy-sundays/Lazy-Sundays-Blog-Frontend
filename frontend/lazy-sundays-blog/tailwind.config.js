a52768d178f16384578b3dc7db3d90842a2a4b05
Devin Toms <dtoms@vt.edu>
2023-05-25 23:01:30 -0400

    updated dark mode toggle to use icons, simple animation

--- frontend/lazy-sundays-blog/tailwind.config.js
+++ frontend/lazy-sundays-blog/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      animation: {
+        'blink': 'blink 1s cubic-bezier(0.34, 1.56, 0.64, 1)',
+      },
       colors: {
         "1-primary": "#048BA8",
         "1-secondary": "#F18F01",
@@ -11,6 +14,16 @@ module.exports = {
         "2-primary": "#E8998D",
         "2-secondary": "#6C9A8B",
         "2-tertiary": "#EED2CC",
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { 
+            'opacity': '0',
+          },
+          '50%': { 
+            'opacity': '1' ,
+          },
+        },
       },
     },
   },
