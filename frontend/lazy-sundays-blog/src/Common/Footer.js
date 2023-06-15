621e81738067c36f3364b82012189589fd647536
Jenny Leidig <jennymaeleidig@gmail.com>
2023-06-15 19:55:50 -0400

    edited footer design

--- frontend/lazy-sundays-blog/src/Common/Footer.js
+++ frontend/lazy-sundays-blog/src/Common/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
     let rnd = 1;
@@ -13,7 +15,7 @@ export default function Footer() {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-4">
                 <div className="flex items-center justify-between">
                     <a href="/" className="flex items-center mb-4 sm:mb-0">
-                    <span className="self-center text-md font-semibold whitespace-nowrap">Lazy Sundays Blog</span>
+                        <span className="self-center text-md font-semibold whitespace-nowrap">Lazy Sundays Blog</span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-4 text-sm font-medium sm:mb-0">
                         {links.map((link) => {
@@ -25,9 +27,17 @@ export default function Footer() {
                         })}
                     </ul>
                 </div>
-                <hr className="my-6 mx-auto lg:my-8"/>
-                <span className="block text-sm text-center mb-2">Est. 2023.</span>
-                <span className="block text-sm text-center">Designed by Devin Toms and Jenny Leidig</span>
+                <div className="inline-flex items-center justify-center w-full">
+                    <hr className="w-4/5 h-1 my-8 bg-gray-200 border-0 rounded dark:bg-gray-700"/>
+                    <div className="absolute px-6 -translate-x-1/2 bg-stone-100 left-1/2 dark:bg-slate-900">
+                        <FontAwesomeIcon icon={faSun} className="bg-transparent"/>
+                    </div>
+                </div>
+                <span className="block text-xs text-center">
+                    <div className="mb-2">Est. 2023.</div>
+                    {/* TODO: add links to names so that they link to author page */}
+                    <div className="">Designed by Devin Toms and Jenny Leidig</div>
+                </span>
             </div>
         </footer>
     );
