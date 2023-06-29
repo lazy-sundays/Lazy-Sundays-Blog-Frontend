0296295c68afee27d918270cfd29b072db8c49a5
dtoms <dtoms@vt.edu>
2023-06-29 00:59:42 -0400

    implemented basic contributor card styling

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 import clive_img from "../media/temp/clive.jpg";
 import jill_img from "../media/temp/jill.jpg";
@@ -73,7 +75,7 @@ export default function AboutUs() {
 Minim delicata adipiscing sit id, ea elitr impetus delenit mea, graecis fuisset eum in. Eam ne quaeque nominavi, ad eum principes imperdiet hendrerit, doctus pertinacia mea ad. Fuisset percipit eum ut, cum at quot sonet evertitur. Ne nec veri vitae qualisque, nec propriae accumsan in, est no quot lorem adipiscing. Eu mutat causae volumus vis. Cu regione civibus vim, an nec habeo virtute blandit.
                 </p>
             </div>
-            <div className="w-full lg:w-3/12">
+            <div className="w-auto">
                 <h1 className="text-2xl md:text-3xl font-bold uppercase mb-5">
                     Contact Us
                 </h1>
@@ -86,25 +88,46 @@ Minim delicata adipiscing sit id, ea elitr impetus delenit mea, graecis fuisset 
                     </li>
                 </ul>
             </div>
-            <div className="w-full ">
+            <div className="w-full"> 
                 <h1 className="text-2xl md:text-3xl font-bold uppercase mb-5">
                     List of Contributors
                 </h1>
-                <ul className="flex flex-wrap gap-y-8 mx-2 sm:mx-8">
+                <ul className="flex flex-wrap gap-6 mx-2 sm:mx-8">
                     {authorsPlaceholderResponse.data.map((author) => {
                         return (
-                            <li className="flex w-full sm:w-1/2">
-                                <img src={author.attributes.avatar.url}
-                                    className={"max-w-[96px] max-h-[96px] mr-4 rounded-full border-2 border-stone-500/75"}
-                                />
+                            <li className="flex group relative w-full p-2 lg:basis-[calc(50%-1.5rem)] bg-white dark:bg-slate-800 border border-slate-700/80 rounded-md">
+                                {/* TODO: turn into seperate component */}
+                                <div className="group-hover:z-10">
+                                    <img src={author.attributes.avatar.url}
+                                        className={" max-w-[96px] max-h-[96px] mr-4 rounded-full border-2 border-stone-500/75"}
+                                    />
+                                </div>
                                 <div className="w-1/2 flex flex-col justify-center">
-                                    <h3 className="w-full text-xl font-semibold">
+                                    <h3 className="w-full text-xl font-semibold group-hover:animate-fancy-shadow-1">
                                         {author.attributes.name}
                                     </h3>
                                     <span className="text-lg">
                                         Contributor since {author.attributes.createdAt}
                                     </span>
                                 </div>
+                                <div className="flex w-auto h-auto grow justify-center items-center">
+                                    <FontAwesomeIcon icon={faAngleRight} />
+                                </div>
+                                <div id="rectangle" 
+                                    className="-z-10 absolute w-full h-full p-2 bg-1-primary rounded-md -translate-x-2 -translate-y-2
+                                        transition ease-in delay-100 group-hover:transition group-hover:ease-out group-hover:delay-200
+                                        group-hover:-translate-x-1 group-hover:-translate-y-1"
+                                />
+                                <div id="rectangle" 
+                                    className="-z-20 absolute w-full h-full p-2 bg-1-secondary rounded-md -translate-x-2 -translate-y-2
+                                        transition ease-in-out delay-150
+                                        group-hover:-translate-x-0.5 group-hover:-translate-y-0.5"
+                                />
+                                <div id="rectangle" 
+                                    className="-z-30 absolute w-full h-full p-2 bg-1-tertiary rounded-md -translate-x-2 -translate-y-2
+                                        transition ease-in delay-200 group-hover:transition group-hover:ease-out group-hover:delay-100
+                                        group-hover:translate-x-0 group-hover:translate-y-0"
+                                />
                             </li>
                         );
                     })}
--- src/Common/Navbar.js
+++ src/Common/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import logo from "../media/logo.svg";
 import ThemeSwitch from "./ThemeSwitch";
