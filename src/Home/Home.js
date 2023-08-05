de5084ff3c0470d19523dbe0c4550f039cf9d8ed
dtoms <dtoms@vt.edu>
2023-08-05 12:21:44 -0400

    integrated recent articles on homepage

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,64 +1,36 @@
-import React, { Component } from "react";
-import ArticlePlaque from "../Article/ArticlePlaque";
+import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
 import Article from "../Article/Article";
 
 export default function Home() {
-    const example_date = new Date().toLocaleDateString("en-US", {
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    });
-    const featuredArticlePlaceholderResponse = {
-        "data": {
-                "id": 16,
-                "attributes": {
-                    "title": "This Leather Glove Looks So Good",
-                    "hero": {
-                        "data": {
-                            "url": "https://media.discordapp.net/attachments/201724822877634560/1124420347471605830/1890dc2f17b64-screenshotUrl.jpg", 
-                            "alternativeText": "A hand wearing a leather glove rested in a fist on a wooden table",
-                        },
-                    },
-                    "tagline": "This leather glove from Final Fantasy XVI looks incredible -- here's a breakdown.",
-                    "authors": {
-                        "data": [
-                            {
-                                "id": 16,
-                                "avatar": {
-                                    "data": {
-                                        "url": "https://cdnen.samurai-gamers.com/wp-content/uploads/2023/06/14093419/Final-Fantasy-XVI-FF16-Clive-Rosfield-Young-150x150.jpg",
-                                        "alternativeText": "string",
-                                    },
-                                },
-                                "name": "Clive Rosfield",
-                            },
-                        ],
-                    },
-                    "createdAt": example_date,
-                    "tags": {
-                        "data": [
-                            {
-                                "id": 0,
-                                "attributes": {
-                                    "tag": "example",
-                                },
-                            }
-                        ],
-                    },
-                    "readTime": 999,
-                },
-                "meta": {
-                    "availableLocales": []
+    const [loadRecentArticles, setLoadRecentArticles] = useState(true);
+    const [recentArticles, setRecentArticles] = useState([]);
+
+    async function getRecentArticles() {
+        try {
+            const response = await axios.get(
+                
+                process.env.REACT_APP_URI_ROOT+"/api/articles?sort[0]=publishedAt:desc&fields[0]=publishedAt&fields[1]=title&fields[2]=slug&pagination[page]=1&pagination[pageSize]=10&populate[0]=authors",
+                {
+                    headers: {
+                        Authorization: 'Bearer '+process.env.REACT_APP_STRAPI_API_KEY
+                    }
                 }
-        },
-        "meta": {}
+            );
+            // console.log(response.data.data);
+            setRecentArticles(response.data.data);
+            // return response.data;
+        } catch (error) {
+            // TODO: navigate to error page
+            console.log(error);
+        } finally{
+            setLoadRecentArticles(false);
+        }
     };
 
-    const recentArticlesPlaceholderResponse = [
-        "This Leather Glove Looks So Good",
-        "Let's Save the World Using the Power of Friendship",
-        "Man Eats Food Really Fast, Gets Into Bar Fight"
-    ];
+    useEffect(() => {
+        if (loadRecentArticles) getRecentArticles();
+    }, []);
 
     return (
         <article id="pg-content" className="flex flex-wrap gap-y-10 px-4 pt-5 pb-10 sm:px-14 lg:px-20">
@@ -82,23 +54,23 @@ export default function Home() {
                     Most Recent Articles
                 </h1>
                 <ul>
-                    {recentArticlesPlaceholderResponse.map( (name, i) => {
+                    {!loadRecentArticles && recentArticles.map( (article, i, all) => {
                         return (
                             <>
-                            <li className={`relative group ${(i === 0 ? "pb-6" : "py-6")} px-2 sm:px-8`}>
+                            <li className={`relative group ${("py-6")} mx-2 sm:px-8 hover:bg-stone-100 hover:dark:bg-slate-700`}>
                                 <span className="text-xs text-1-primary">
-                                    {(new Date()).toLocaleDateString( "en-US",
+                                    {(new Date(article.attributes.publishedAt)).toLocaleDateString( "en-US",
                                         {month: '2-digit', day: '2-digit'}
                                     )}
                                 </span>
                                 <span className="ml-2 text-md font-semibold group-hover:underline group-hover:decoration-1-primary group-hover:decoration-2">
-                                    {name}
+                                    {article.attributes.title}
                                 </span>
-                                <a href={`/articles/${name}`} aria-label="go to author's bio'">
+                                <a href={`/articles/${article.attributes.slug}`} aria-label="go to author's bio'">
                                     <span className="absolute inset-0" aria-hidden/>
                                 </a>
                             </li>
-                            { (i+1 < recentArticlesPlaceholderResponse.length) &&
+                            {(i < all.length-1) &&
                                 <hr className="mx-2 dark:border-white/25 border-slate-900/25" />
                             }
                             </>
