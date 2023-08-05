e1074cae2dc824d480981ba756673b803256cdaf
Jenny Leidig <jennymaeleidig@gmail.com>
2023-08-04 20:42:05 -0400

    rebased and added files for random article connection

--- src/Common/Footer.js
+++ src/Common/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSun } from '@fortawesome/free-solid-svg-icons'
+import RandomArticle from "./RandomArticle";
 
 export default function Footer() {
-    let rnd = 1;
     let links = [
-        {key: 0, name: "Random Article", href: `/articles/${rnd}`},
+        {key: 0, name: "Random Article", href: `/articles/`},
         {key: 1, name: "The Archives", href: "/the-archives"},
         {key: 2, name: "About Us", href: "/about-us"},
     ];
@@ -19,6 +19,11 @@ export default function Footer() {
                     </a>
                     <ul className="flex flex-wrap items-center justify-center mb-4 text-sm font-medium sm:mb-0">
                         {links.map((link) => {
+                            if (link.key === 0){
+                                return(
+                                    <RandomArticle hrefRootPath={link.href} displayName={link.name} className="ml-4 hover:underline hover:decoration-1-primary hover:decoration-2 md:ml-6"/>
+                                );
+                            }
                             return (
                                  <li>
                                     <a href={link.href} className="ml-4 hover:underline hover:decoration-1-primary hover:decoration-2 md:ml-6">{link.name}</a>
