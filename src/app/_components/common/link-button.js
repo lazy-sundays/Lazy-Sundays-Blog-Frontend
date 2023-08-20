06ee76ee501528a44b017be29fa5eeff34d54e97
dtoms <dtoms@vt.edu>
2023-08-20 12:32:49 -0400

    updated LinkButton to match Button props

--- src/app/_components/common/link-button.js
+++ src/app/_components/common/link-button.js
@@ -2,7 +2,13 @@
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
-export default function LinkButton({ href, ariaLabel, children, className = ``, disabled = false}) {
+export default function LinkButton({ 
+    href, 
+    ariaLabel = 'hyperlink', 
+    children, 
+    className = ``, 
+    disabled = false
+}) {
     const [loading, setLoading] = useState(false);
     const router = useRouter();
 
@@ -27,19 +33,19 @@ export default function LinkButton({ href, ariaLabel, children, className = ``, 
             { !disabled && 
                 <>
                     <div aria-hidden
-                        className="-z-10 absolute w-full h-full p-4 bg-accentprimary rounded-md top-0 left-0 text-bgprimary">
+                        className={`-z-10 absolute w-full h-full p-4 bg-accentprimary rounded-md top-0 left-0 text-bgprimary ${className}`}>
                             {/* "shadow" text allows smooth transition of color between all possible states */}
                             {loading ? <>Loading...</> : children}
                     </div>
                     <div aria-hidden
-                        className="-z-20 absolute w-full h-full p-4 bg-accentsecondary rounded-md top-0 left-0
+                        className={`-z-20 absolute w-full h-full p-4 bg-accentsecondary rounded-md top-0 left-0
                             transition ease-in delay-150 group-hover:transition group-hover:ease-out group-hover:delay-150
-                            group-hover:translate-x-0.5 group-hover:translate-y-0.5"
+                            group-hover:translate-x-0.5 group-hover:translate-y-0.5 ${className}`}
                     />
                     <div aria-hidden
-                        className="-z-30 absolute w-full h-full p-4 bg-accenttertiary rounded-md top-0 left-0
+                        className={`-z-30 absolute w-full h-full p-4 bg-accenttertiary rounded-md top-0 left-0
                             transition ease-in delay-200 group-hover:transition group-hover:ease-out group-hover:delay-100
-                            group-hover:translate-x-1 group-hover:translate-y-1"
+                            group-hover:translate-x-1 group-hover:translate-y-1 ${className}`}
                     />
                 </>
             }
