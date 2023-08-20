cec4a01dcddb26c07aa6784b4e0ce489b7b8cd24
dtoms <dtoms@vt.edu>
2023-08-20 19:24:05 -0400

    updated ContributorPlaque to use LinkButton; reworked LinkButton to use Link

--- src/app/_components/common/link-button.js
+++ src/app/_components/common/link-button.js
@@ -1,54 +1,51 @@
 "use client"
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import Link from 'next/link';
 
-export default function LinkButton({ 
+export default function LinkButton({
+    as: Component = "div",
     href, 
     ariaLabel = 'hyperlink', 
     children, 
     className = ``, 
     disabled = false
 }) {
-    const [loading, setLoading] = useState(false);
-    const router = useRouter();
 
     return (
-        <button 
+        <Component
             className={
-                `relative group w-fit p-4 sm:px-10 md:px-14 lg:px-120 m-auto rounded-md border border-bgsecondary font-semibold text-lg bg-bgsecondary
+                `relative group w-fit p-4 m-auto rounded-md border border-bgsecondary font-semibold text-center text-lg bg-bgsecondary
                 ${disabled && `opacity-30`}
                 ${!disabled && `transition-transform ease-in delay-100 hover:ease-out group-hover:delay-200 hover:-translate-x-1 hover:-translate-y-1`}
                 ${className}`
             }
-            onClick={() => {
-                setLoading(true);
-                router.push(href)
-            }}
             aria-label={ariaLabel || href}
             disabled={disabled}
         >
             <span className={!disabled && `group-hover:transition-none group-hover:invisible transition-[visibility] delay-200 ease-linear`}>
-                {loading ? <>Loading...</> : children}
+                {children}
             </span>
             { !disabled && 
                 <>
                     <div aria-hidden
-                        className={`-z-10 absolute w-full h-full p-4 bg-accentprimary rounded-md top-0 left-0 text-bgprimary ${className}`}>
+                        className={`-z-10 absolute w-full h-full p-4 bg-accentprimary rounded-md top-0 left-0 text-bgprimary`}>
                             {/* "shadow" text allows smooth transition of color between all possible states */}
-                            {loading ? <>Loading...</> : children}
+                            {children}
                     </div>
                     <div aria-hidden
                         className={`-z-20 absolute w-full h-full p-4 bg-accentsecondary rounded-md top-0 left-0
                             transition ease-in delay-150 group-hover:transition group-hover:ease-out group-hover:delay-150
-                            group-hover:translate-x-0.5 group-hover:translate-y-0.5 ${className}`}
+                            group-hover:translate-x-0.5 group-hover:translate-y-0.5`}
                     />
                     <div aria-hidden
                         className={`-z-30 absolute w-full h-full p-4 bg-accenttertiary rounded-md top-0 left-0
                             transition ease-in delay-200 group-hover:transition group-hover:ease-out group-hover:delay-100
-                            group-hover:translate-x-1 group-hover:translate-y-1 ${className}`}
+                            group-hover:translate-x-1 group-hover:translate-y-1`}
                     />
                 </>
             }
-        </button>
+            <Link href={href} aria-label={`go to ${href}`}>
+                <span className="absolute inset-0" aria-hidden/>
+            </Link>
+        </Component>
     );
 }
