81bef468397f0d4189c920d8e15df1d2bf3d67f4
dtoms <dtoms@vt.edu>
2023-09-02 12:07:01 -0400

    fixed an issue where the back to top button would overlap with page elements at some scroll lengths

--- src/app/_components/common/back-to-top.js
+++ src/app/_components/common/back-to-top.js
@@ -12,10 +12,10 @@ export default function BackToTop(){
     const toggleVisible = () => {
         if (!isBrowser()) return;
         const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 300){
+        if (scrolled > window.innerHeight){
           setVisible(true)
         } 
-        else if (scrolled <= 300){
+        else if (scrolled <= window.innerHeight){
           setVisible(false)
         }
       };
@@ -35,7 +35,7 @@ export default function BackToTop(){
     
     return (
       <div className={`${visible ? 'sticky' : "invisible"} w-full flex justify-end bottom-0 pointer-events-none`}>
-        <div className={`-translate-x-6 -translate-y-6 sm:-translate-x-10 sm:-translate-y-10`}
+        <div className={`mt-6 sm:mt-10 -translate-x-6 -translate-y-6 sm:-translate-x-10 sm:-translate-y-10`}
         >
           <button onClick={scrollToTop} className={`p-4 pointer-events-auto rounded-full bg-accentprimary text-bgsecondary hover:opacity-75`}>
             <FontAwesomeIcon className={`fa-lg fa-fw`} icon={faArrowUp} />
