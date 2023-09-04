d7b8b3a6f35c4cb11e51d1229fa909e029d6ef47
dtoms <dtoms@vt.edu>
2023-09-04 00:16:32 -0400

    hotfixed an issue cause sitemap.xml to not contain static pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL_ROOT || 'https://alazysunday.com',
+    additionalPaths: async () => {
+        const staticPaths = ['/', '/about-us', '/the-archives', '/random-article']
+        const result = staticPaths.map((path) => {
+            return ({
+            loc: path,
+            changefreq: 'daily',
+            priority: 0.7,
+            lastmod: new Date().toISOString(),
+            })
+        });
+        return result;
+    },
     exclude: [
         `/secret`,
         `/server-sitemap.xml`,
