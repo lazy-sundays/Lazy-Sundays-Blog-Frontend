77d87b75ad097ac25da05f54d3a234b4f9be9c77
Jenny Leidig <jennymaeleidig@gmail.com>
2023-09-07 22:36:02 -0400

    implemented tag based on demand revaldiation

---
+++ src/app/_lib/api-tags.js
@@ -0,0 +1,14 @@
+export const apiTags ={
+
+    article: "article",
+    author: "author",
+    contactInfo: "contact-info",
+    linkTree: "link-tree",
+    tag: "tag",
+    aboutUs: "about-us",
+    contactUs: "contact-us",
+    featuredArticle:"featured-article",
+    mostRecentArticles: "most-recent-articles",
+    listOfContr: "list-of-contrib",
+    numContrib: "num-contrib",
+};
--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,21 +1,62 @@
 import { NextResponse } from 'next/server'
-import { revalidateTag, revalidatePath } from 'next/cache'
+import { revalidateTag} from 'next/cache'
+import {apiTags}  from '@/app/_lib/api-tags';
 
-
-export const fetchCache = "force-no-store";
 export const runtime="edge"
 
 export async function POST(request) {
 
-    const secret = request.nextUrl.searchParams.get('secret');
+    const re = new RegExp("^(?<bearer>Bearer\\s)(?<token>[a-zA-Z0-9-_=]+)$", "g");
+    const authHeader = (request.headers.get("authorization"));
 
-    if (secret !== process.env.STRAPI_WEBHOOK_SECRET) {
+    //verify authorization header exists
+    if (!authHeader) {
+        return new NextResponse('No authorization header found', { status: 401 });
+    }
+
+    //ensure that the header is in correct format
+    const found = re.exec(authHeader);
+    if (found === null){
+        return new NextResponse('Invalid authorization type', { status: 401 });
+    }
+
+    //verify the secret matches
+    if (found.groups.token != process.env.STRAPI_WEBHOOK_SECRET){
         return new NextResponse('Invalid secret', { status: 401 });
     }
 
     const body = await request.json();
-    console.log(body.event)
-    console.log(body.model);
+    
+    //revalidate based on the type of model that was updated
+    switch(body.model){
+        case (apiTags.article):
+            revalidateTag(apiTags.article + body.entry.slug);
+            revalidateTag(apiTags.numContrib);
+            revalidateTag(apiTags.mostRecentArticles);
+            break;
+        case(apiTags.author):
+            revalidateTag(apiTags.author + body.entry.slug);
+            revalidateTag(apiTags.listOfContr);
+            break;
+        case(apiTags.contactInfo):
+            revalidateTag(apiTags.contactInfo);
+            break;  
+        case(apiTags.linkTree):
+            break;
+        case(apiTags.tag):
+            break;
+        case(apiTags.aboutUs):
+            revalidateTag(apiTags.aboutUs);
+            break;
+        case(apiTags.contactUs):
+            revalidateTag(apiTags.contactUs);
+            break;
+        case(apiTags.featuredArticle):
+            revalidateTag(apiTags.featuredArticle);
+            break;
+        default:
+            return new NextResponse(null, { status: 200 });
+    }
 
     return new NextResponse(null, { status: 200 });
     
