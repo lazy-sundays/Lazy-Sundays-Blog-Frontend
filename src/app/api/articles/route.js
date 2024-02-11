38a265ac80ccd9139364ae23620dfe534ea644c8
Devin Toms <devintoms01@gmail.com>
2024-02-10 20:09:22 -0500

    implemented basic list functionality

--- src/app/api/articles/route.js
+++ src/app/api/articles/route.js
@@ -8,7 +8,7 @@ export async function GET(req){
     const page = searchParams.get('page')
     const pageSize = searchParams.get('pageSize')
 
-    return await fetch(`${process.env.STRAPI_URI_ROOT}/api/articles?sort[0]=publishedAt:desc&fields[0]=publishedAt&fields[1]=title&fields[2]=slug&pagination[page]=${page}&pagination[pageSize]=${pageSize}`, 
+    return await fetch(`${process.env.STRAPI_URI_ROOT}/api/articles?sort[0]=publishedAt:desc&fields[0]=publishedAt&fields[1]=title&fields[2]=slug&fields[3]=tagline&fields[4]=hero&fields[5]=heroAltText&fields[6]=readTime&populate[authors][fields][0]=slug&populate[authors][fields][1]=name&pagination[page]=${page}&pagination[pageSize]=${pageSize}`, 
     { 
         method: "GET",
         headers: {
