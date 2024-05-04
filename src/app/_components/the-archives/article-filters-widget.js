db5f87bc27aa5260cfffbed6df23bdf565eaacdc
Devin Toms <dtoms@vt.edu>
2024-05-04 17:07:19 -0400

    fixed spacing

--- src/app/_components/the-archives/article-filters-widget.js
+++ src/app/_components/the-archives/article-filters-widget.js
@@ -1,14 +1,15 @@
+import Construction from "../construction";
 
 
 export default function ArticleFiltersWidget({ filters }) {
 
     return (
-        <div className="p-2 rounded-md flex flex-col bg-bgsecondary">
-            <h2 className="uppercase font-bold">
+        <div className="p-2 mb-4 rounded-md flex flex-col bg-bgsecondary">
+            <h2 className="text-2xl uppercase font-bold">
                 Refine Results
             </h2>
             <div className="mx-2">
-                Coming soon!
+                <Construction />
             </div>
         </div>
     );
