0445ec6666d9f712dd46747e3cbe813187f3e5d3
jennymaeleidig <jennymaeleidig@gmail.com>
2026-02-23 15:46:00 -0500

    switch from cloudinary

--- next.config.js
+++ next.config.js
@@ -1,62 +1,62 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-          port: '',
-          pathname: '/dqcr2lwws/image/upload/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'pbs.twimg.com',
-          port: '',
-          pathname: '/media/**',
-        },
-      ],
-    },
-    async redirects() {
-        return [
-          {
-            source: '/articles',
-            destination: '/the-archives',
-            permanent: true,
-          },
-          {
-            source: '/authors',
-            destination: '/about-us',
-            permanent: true,
-          },
-        ]
-    },
-    webpack(config) {
-      // Grab the existing rule that handles SVG imports
-      const fileLoaderRule = config.module.rules.find((rule) =>
-        rule.test?.test?.('.svg'),
-      )
-  
-      config.module.rules.push(
-        // Reapply the existing rule, but only for svg imports ending in ?url
-        {
-          ...fileLoaderRule,
-          test: /\.svg$/i,
-          resourceQuery: /url/, // *.svg?url
-        },
-        // Convert all other *.svg imports to React components
-        {
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          resourceQuery: { not: /url/ }, // exclude if *.svg?url
-          use: [{loader: '@svgr/webpack', options: { icon: true }}],
-        },
-      )
-  
-      // Modify the file loader rule to ignore *.svg, since we have it handled now.
-      fileLoaderRule.exclude = /\.svg$/i
-  
-      return config
-    },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "file.garden",
+        port: "",
+        pathname: "/aZO3VlhL62o8DudL/lazy-sundays-blog/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        port: "",
+        pathname: "/media/**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/articles",
+        destination: "/the-archives",
+        permanent: true,
+      },
+      {
+        source: "/authors",
+        destination: "/about-us",
+        permanent: true,
+      },
+    ];
+  },
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg"),
+    );
 
-module.exports = nextConfig
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
